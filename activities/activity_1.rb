f9b9b723eaaa144d06257779ad022e30dd4a238d
Rex <johnrex412@gmail.com>
2023-02-28 14:40:24 +0800

    Activity 1: Create a script that returns the average of adult age array.

--- activities/activity_1.rb
+++ activities/activity_1.rb
@@ -2,3 +2,20 @@
 #  Write a function that takes an Array of ages,
 #  the function will return the average age of adults.
 #  Print on console the result.
+
+data = [23, 21, 24, 51, 109, 17]
+def getAverage (ages)
+  totalAge = 0
+  totalIndex = 0
+
+  ages.each do |age|
+    if age > 17
+      totalAge += age
+      totalIndex += 1
+    end
+  end
+
+  return totalAge/totalIndex.to_f
+end
+
+print "The average age of adults is: #{getAverage(data)} \n"
