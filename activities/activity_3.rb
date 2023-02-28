d0d86cfdb954b4864b0116ed927385daf8280eef
Rex <johnrex412@gmail.com>
2023-02-28 15:49:40 +0800

    Activity 3: Create a script that returns an array of age difference of current age and previous age of an age array.

--- activities/activity_3.rb
+++ activities/activity_3.rb
@@ -2,3 +2,18 @@
 #  Write a function that takes an array of ages.
 #  The function will return an array of age differences between the 1st and 2nd age, 2nd and 3rd  .. so on and so forth.
 #  Print on console the result.
+
+ages = [12, 42, 23, 24]
+
+def getDifference (data)
+  diff = Array.new
+  data.each_with_index do |item, index|
+    if (index > 0)
+      diff.push(item - data[index - 1])
+    end
+  end
+
+  return diff
+end
+
+p getDifference(ages)
