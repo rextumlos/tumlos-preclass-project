9b634403be0d09eb6e0ac36a7df4f9b92e1e2228
Rex <johnrex412@gmail.com>
2023-02-28 15:50:28 +0800

    Activity 4: Create a script that returns non-duplicate age in an array.

--- activities/activity_4.rb
+++ activities/activity_4.rb
@@ -2,3 +2,30 @@
 #  Write a function that takes an array of ages.
 #  The function will return an Array of ages that you could only see one time.
 #  Print on console the result.
+
+ages = [31, 21, 19, 24, 31, 19, 29]
+
+def checkDup (check, array, dataIndex)
+  array.each_with_index do |item, index|
+    if (index != dataIndex)
+      if item == check
+        return true
+      end
+    end
+  end
+
+  return false
+end
+
+def oneTime (data)
+  result = Array.new
+  data.each_with_index do |item, index|
+    if !checkDup(item, data, index)
+      result.push(item)
+    end
+  end
+
+  return result
+end
+
+p oneTime(ages)
