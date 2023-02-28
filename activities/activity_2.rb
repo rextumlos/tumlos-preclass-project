61f9168cf14a2d3212f0488c5ccc3cbcb33a933a
Rex <johnrex412@gmail.com>
2023-02-28 14:48:11 +0800

    Activity 2: Create a script that returns the fullname of hash name input.

--- activities/activity_2.rb
+++ activities/activity_2.rb
@@ -3,3 +3,15 @@
 #  Values will be your respective names.
 #  Write a function that takes a Hash and returns the full name.
 #  Print on console the result.
+
+my_name = Hash[
+  first_name: "John Rexpearl",
+  middle_name: "Pascual",
+  last_name: "Tumlos",
+]
+
+def getFullName (name)
+  return "#{name[:first_name]} #{name[:middle_name]} #{name[:last_name]} \n"
+end
+
+print getFullName(my_name)
